13b55b5dcb04066fbda2933de2b0018ce2db1a61
samirjabib <samirdev9669@gmail.com>
2023-02-14 10:17:09 -0500

    add

---
+++ react-with-typescript/src/App.tsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+
+  return (
+    <div className="App">
+        <h1 className="text-5xl">introduccion a typescript</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ react-with-typescript/src/typescript/BasicTypes.jsx
@@ -0,0 +1,7 @@
+export const BasicTypes = () => {
+    return(
+        <div>
+            Types Basic
+        </div>
+    )
+}
