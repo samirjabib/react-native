9353a7c9a9f685b1a58d9218dde4c52d5a7a13ba
samirjabib <samirdev9669@gmail.com>
2023-02-14 11:13:46 -0500

    reducer with typescript

--- react-with-typescript/src/components/Login.tsx
+++ react-with-typescript/src/components/Login.tsx
@@ -1,4 +1,4 @@
-import { useReducer } from "react"
+import { useEffect, useReducer } from "react"
 
 const initialState = {
     validate: true,
@@ -14,18 +14,32 @@ interface AuthState {
     name:string;
 }
 
-type AuthAction = { type:'logout'}
+type AuthAction = 
+    |{ type:'logout'}
+    |{type:'login', payload:LoginPayload}
+
+type LoginPayload = {
+    username:string,
+    name:string
+}
 
 
 const authReducer = (state:AuthState, action:AuthAction) :AuthState => {
     switch (action.type) {
         case 'logout':
-            
             return{
                 validate:false,
                 token:null,
                 username:'',
                 name:'',
+            }
+        case 'login':
+            const { name, username} = action.payload
+            return{
+                token:'abc',
+                validate:false,
+                name,
+                username
             }
     
         default:
@@ -39,43 +53,84 @@ export const Login = () => {
     const [state, dispatch] = useReducer(authReducer, initialState)
 
 
+    useEffect(() => {
+      setTimeout( () => {
+        dispatch({ type: 'logout'})
+      }, 1500)
+    }, [])
+
+
+    const login = () => {
+        dispatch({
+            type:'login',
+            payload:{
+                name:'samir',
+                username:'samirdev9669'
+            }
+        })
+    }
+
+    const logout = () => {
+        dispatch({
+            type:'logout'
+        })
+    }
+    
+
+    if(state.validate){
+        return (
+            <>
+                <div
+                className="bg-blue-500 w-full h-36 items-center justify-center text-black font-bold flex"
+                >
+                Validando....
+                </div>
+            </>
+        )
+    }
+
 
   return (
     <div className="h-screen flex items-center flex-col gap-y-4">
 
         <h3 className="text-2xl font-bold mb-4">Login</h3>
 
-        <div
-            className="bg-blue-500 w-full h-36 items-center justify-center text-black font-bold flex"
-        >
-            Validando....
-        </div>
-
-        <div
-            className="bg-red-500 w-full h-36 items-center justify-center text-black font-bold flex"
-        >
-            No autenticado
-        </div>
-
-        <div
-            className="bg-green-500 w-full h-36 items-center justify-center text-black font-bold flex"
-        >
-            autenticado
-        </div>
-
+        {
+            (state.token)
+                ?    
+                    <div
+                        className="bg-green-500 w-full h-36 items-center justify-center text-black font-bold flex"
+                    >
+                        autenticado como  {state.name}
+                    </div>    
+                :
+                    <div
+                    className="bg-red-500 w-full h-36 items-center justify-center text-black font-bold flex"
+                    >
+                        No autenticado
+                    </div>
+        }
 
         <div className="flex flex-row gap-4">
-            <button
-                className="px-4 py-2 bg-yellow-300"
-            >
-                Login
-            </button>
-            <button
-                className="px-4 py-2 bg-blue-300"
-            >
-                Logout
-            </button>
 
+        {   
+            (state.token)
+                ?    
+                    <button
+                        className="px-4 py-2 bg-red-500"
+                        onClick={logout}
+                    >
+                        Logout
+                    </button>
+                       
+                :
+                    <button
+                        className="px-4 py-2 bg-green-500"
+                        onClick={login}
+                    >
+                        Login
+                    </button>      
+        }
         </div>
 
     </div>
